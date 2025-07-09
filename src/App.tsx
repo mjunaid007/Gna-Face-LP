9ab0dd7b5ffe90dc34b5ce3dc1c8bf524d3d4b47
M Junaid Nasir <156563939+mjunaid007@users.noreply.github.com>
2025-07-09 11:47:12 +0530

    Remove Hero section completely

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
 import WhyChoose from './components/WhyChoose';
@@ -14,7 +13,6 @@ function App() {
   return (
     <div className="min-h-screen">
       <Header />
-      <Hero />
       <Services />
       <About />
       <WhyChoose />
